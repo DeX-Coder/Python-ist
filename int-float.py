00e2662c5dfa78506429ea7c939297483d656bda
DeX-Coder <dehimangshu03@gmail.com>
2023-11-07 10:28:19 +0530

    Nailed! Numeric Data in python
    * Numbers intro
    * Arithmetic Operators
    * Comparison Operators
    * Type casting of string form numbers to integer.

---
+++ int-float.py
@@ -0,0 +1,44 @@
+
+num = 15 #(int)
+num = 3.14 #(float)
+
+print(type(num))
+
+# Arithmetic Operators
+# Add:              3 + 2
+# Subtract:         3 - 2
+# Multiply:         3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus:          3 % 2
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print()
+
+
+# Comparisons:
+# Equal:                3 == 2
+# Not Equal:            3 != 2
+# Greater Than:         3 > 2
+# Less Than:            3 < 2
+# Greater or Equal:     3 >= 2
+# Less or Equal:        3 <= 2
+
+print(3 == 2)
+print(3 != 2)
+print(3 > 2)
+print(3 < 2)
+print(3 >= 2)
+print(3 <= 2)
+print()
+
+num_1 = '100'
+num_2 = '200'
+print(int(num_1) + int(num_2)) # Type Casting Done from String to Integers!
