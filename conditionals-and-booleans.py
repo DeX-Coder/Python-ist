ad5a3bd4ae121e79dcd75679cd0de8b108236973
DeX-Coder <dehimangshu03@gmail.com>
2023-11-13 10:06:58 +0530

    Updated! ¯\_(ツ)_/¯ Uncommented lines of code for direct execution.

--- conditionals-and-booleans.py
+++ conditionals-and-booleans.py
@@ -8,34 +8,34 @@
 # Less or Equal:        <= 
 # Object identity:      is
 
-# language = 'JavaScript'
+language = 'JavaScript'
 
-# if language == 'Python':
-#     print('Language is Python')
+if language == 'Python':
+    print('Language is Python')
 
-# elif language == 'Java':
-#     print('Language is Java')
+elif language == 'Java':
+    print('Language is Java')
 
-# elif language == 'JavaScript':
-#     print('Language is JavaScript')
+elif language == 'JavaScript':
+    print('Language is JavaScript')
 
-# else:
-#     print('No Match!')
+else:
+    print('No Match!')
 
 # Just Another way of doing if else: (Available in Python version 3.10 and Onwards!)
 
-# print()
-# print('Doing using match case statement')
-# print()
-# match language:
-#     case 'Python':
-#         print('Language is Python')
-#     case 'Java':
-#         print('Language is Java')
-#     case 'JavaScript':
-#         print('Language is JavaScript')
-#     case _:
-#         print('No match')
+print()
+print('Doing using match case statement')
+print()
+match language:
+    case 'Python':
+        print('Language is Python')
+    case 'Java':
+        print('Language is Java')
+    case 'JavaScript':
+        print('Language is JavaScript')
+    case _:
+        print('No match')
 
 # If one's got their hands dirty with C or C++, then you're tired of adding break statement in
 # the end of every case statement's block. In Python, you don't have to do that. 
@@ -47,18 +47,18 @@
 # or -- either of the statements have to be true
 # not 
 
-# user = 'Admin'
-# logged_in = False
+user = 'Admin'
+logged_in = False
 
-# if user == 'Admin' and logged_in:
-    # print('Admin Page')
-# if user == 'Admin' or logged_in:
-    # print('Admin Page')
-# if not logged_in:
-    # print('Please Log In!')
+if user == 'Admin' and logged_in:
+    print('Admin Page')
+if user == 'Admin' or logged_in:
+    print('Admin Page')
+if not logged_in:
+    print('Please Log In!')
     
-# else:
-    # print('Welcome!')
+else:
+    print('Welcome!')
 # ==============================================================================
 
 # False Values:
@@ -77,11 +77,11 @@ else:
 
 
 
-# a = [1,2,3]
-# # b = [1,2,3]
-# b = a
+a = [1,2,3]
+# b = [1,2,3]
+b = a
 
-# print(id(a))
-# print(id(b))
-# print(a is b)
-# print(id(a) == id(b))
+print(id(a))
+print(id(b))
+print(a is b)
+print(id(a) == id(b))
