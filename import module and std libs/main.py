2d3a8c915ebac1c86e2c6a092363cd532e8b0f0e
DeX-Coder <dehimangshu03@gmail.com>
2023-11-18 11:14:03 +0530

    Completed Importing standard library and modules
    What's covered?
    > from, import, as keywords
    > some std library modules (math, datetime, calendar, os)
    > dunder methods and other standard lib methods.

---
+++ import module and std libs/main.py
@@ -0,0 +1,38 @@
+# import sys
+
+# Std lib module imports
+
+# import random
+# import math
+# import datetime
+# import calendar
+# import os
+# import antigravity
+
+# sys.path.append('C:\\Users\\Himangshu\\Desktop\\my_module')
+# from my_module import find_index, test
+# import my_module
+# import my_module as mm
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = find_index(courses, 'Math')
+# print(index)
+# print(test)
+
+# print(sys.path)
+
+# random_course = random.choice(courses)
+# print(random_course)
+
+# rads = math.radians(90)
+# print(math.sin(rads))
+
+# today = datetime.date.today()
+# print(today)
+
+# print(calendar.isleap(1846))
+
+# print(os.getcwd())
+# print(type(os.getcwd()))
+# print(os.__file__)
